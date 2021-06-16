dcfbbd0667b2af08f84d1acab861eae5ec9e15c2
Rennan <rennanmax80@gmail.com>
2021-06-16 19:53:56 -0300

    Desafio 03 IGNITE

---
+++ README.md
@@ -0,0 +1,28 @@
+## Sobre
+
+Desafio IGNITE modulo 3
+
+Tecnologias usadas no projeto:
+
+- [React](https://pt-br.reactjs.org/)
+- [NextJS](https://nextjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [PrismicCMS](https://prismic.io/docs/technologies/home-prismic-and-nextjs/)
+
+## Início
+
+```bash
+# Clone this project
+$ git clone https://github.com/Rennanmax80/Desafio-Modulo3-IGNITE
+
+# Access
+$ cd SPACETRAVELING
+
+# Install dependencies
+$ yarn
+
+# Run the project
+$ yarn dev
+
+# The server will initialize in the <http://localhost:3000>
+
---
+++ src/pages/post/[slug].tsx
@@ -0,0 +1,221 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+/* eslint-disable react/no-danger */
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { useRouter } from 'next/router';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
+import Header from '../../components/Header';
+import { getPrismicClient } from '../../services/prismic';
+
+import commonStyles from '../../styles/common.module.scss';
+import styles from './post.module.scss';
+
+interface Post {
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    banner: {
+      url: string;
+    };
+    author: string;
+    content: {
+      heading: string;
+      body: {
+        text: string;
+      }[];
+    }[];
+  };
+}
+
+interface PostProps {
+  post: Post;
+  preview: boolean;
+  navigation: {
+    previousPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+    nextPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+  };
+}
+
+export default function Post({
+  post,
+  preview,
+  navigation,
+}: PostProps): JSX.Element {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return (
+      <div className={styles.loading}>
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    total += contentItem.heading.split(' ').length;
+
+    const words = contentItem.body.map(item => item.text.split(' ').length);
+    words.map(word => (total += word));
+    return total;
+  }, 0);
+
+  const readTime = Math.ceil(totalWords / 200);
+
+  const formatedDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
+
+  return (
+    <>
+      <Head>
+        <title>{post.data.title} | spacetraveling.</title>
+      </Head>
+      <Header />
+      <img src={post.data.banner.url} alt="banner" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postHeader}>
+            <h1>{post.data.title}</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                <span>{formatedDate}</span>
+              </li>
+              <li>
+                <FiUser />
+                <span>{post.data.author}</span>
+              </li>
+              <li>
+                <FiClock />
+                <span>{`${readTime} min`}</span>
+              </li>
+            </ul>
+          </div>
+          {post.data.content.map(content => {
+            return (
+              <article key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div
+                  className={styles.postContent}
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </article>
+            );
+          })}
+        </div>
+        <footer className={styles.footer}>
+          {preview && (
+            <aside>
+              <Link href="/api/exit-preview">
+                <a className={styles.closePreviewButton}>
+                  Sair do modo Preview
+                </a>
+              </Link>
+            </aside>
+          )}
+        </footer>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.Predicates.at('document.type', 'post'),
+  ]);
+
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
+  const prismic = getPrismicClient();
+  const { slug } = params;
+  const response = await prismic.getByUID('post', String(slug), {
+    ref: previewData?.ref || null,
+  });
+
+  const previousPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.last_publication_date desc]',
+    }
+  );
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner.url,
+      },
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body],
+        };
+      }),
+    },
+  };
+  return {
+    props: {
+      post,
+      navigation: {
+        previousPost: previousPost?.results,
+        nextPost: nextPost?.results,
+      },
+      preview,
+    },
+  };
+};
